eb8b8ef32d081e42c48fe73718b67d7a65ee357d
Daniel Vilela <dvilelaportela@gmail.com>
2020-03-29 03:52:53 -0300

    Melhoria no Midea e Implementação de JavaScript (btn-menu)

---
+++ javascript/main.js
@@ -0,0 +1,8 @@
+var btnMenu = document.getElementById ('btn-menu');
+var nav = document.getElementById ('menu-secundario');
+
+
+btnMenu.addEventListener ('click' , function () {
+	nav.classList.toggle('mostrar');
+})
+	
