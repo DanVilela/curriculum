120dfc061b34a7d42cf8493afefa0202c40d75c3
Daniel Vilela <dvilelaportela@gmail.com>
2020-03-30 21:06:59 -0300

    Implementação de Hover no Bloquote e Scroll suave no menu- Js

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,5 @@
+// Click do Menu (ida e volta)
+
 var btnMenu = document.getElementById ('btn-menu');
 var nav = document.getElementById ('menu-secundario');
 
@@ -5,4 +7,29 @@ var nav = document.getElementById ('menu-secundario');
 btnMenu.addEventListener ('click' , function () {
 	nav.classList.toggle('mostrar');
 })
-	
+
+
+// Scroll suave para as Sections
+
+const menuItems = document.querySelectorAll('.nav-menu a[href^="#"]');
+
+function getScrollTopByHref(element) {
+	const id = element.getAttribute('href');
+	return document.querySelector(id).offsetTop;
+}
+
+function scrollToPosition(to) {
+window.scroll({top: to, behavior: "smooth",
+	})
+  smoothScrollTo(0, to);
+}
+
+function scrollToIdOnClick(event) {
+	event.preventDefault();
+	const to = getScrollTopByHref(event.currentTarget)- 80;
+	scrollToPosition(to);
+}
+
+menuItems.forEach(item => {
+	item.addEventListener('click', scrollToIdOnClick);
+});
